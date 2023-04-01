76a0fb73678281c3b1b27240f5ec7dc3147c62a2
C# <chashtag.code@gmail.com>
2023-04-01 13:20:19 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -10,9 +12,55 @@ namespace QuizApplication
 {
     public partial class Form1 : Form
     {
+        public static Form1 instance;
+
+        string ColorTheme = "Pink";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ColorTheme = "Blue"; //Themes: Pink, Green, Yellow, Blue
+
+            Theme();
+        }
+
+        private void Theme()
+        {
+            if (ColorTheme == "Green")
+            {
+                ButtonStart.FillColor = Color.OliveDrab;
+                ButtonStart.FillColor2 = Color.GreenYellow;
+                LabelTittle.ForeColor = Color.OliveDrab;
+                LabelDescription.ForeColor = Color.OliveDrab;
+            }
+
+            else if (ColorTheme == "Yellow")
+            {
+                ButtonStart.FillColor = Color.Yellow;
+                ButtonStart.FillColor2 = Color.Olive;
+                LabelTittle.ForeColor = Color.Yellow;
+                LabelDescription.ForeColor = Color.Yellow;
+            }
+
+            else if (ColorTheme == "Blue")
+            {
+                ButtonStart.FillColor = Color.Aquamarine;
+                ButtonStart.FillColor2 = Color.Turquoise;
+                LabelTittle.ForeColor = Color.Aquamarine;
+                LabelDescription.ForeColor = Color.Aquamarine;
+            }
+
+            else
+            {
+                ButtonStart.FillColor = Color.RosyBrown;
+                ButtonStart.FillColor = Color.LightCoral;
+                LabelTittle.ForeColor = Color.RosyBrown;
+                LabelDescription.ForeColor = Color.RosyBrown;
+            }
         }
 
         private void ButtonStart_Click(object sender, EventArgs e)
